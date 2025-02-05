594c42a6550684c1e0c3058fae5e1b79fb547991
Chris Hillery <ceej@couchbase.com>
2025-02-05 02:17:40 -0800

    Updates for 'Modern Cmake' and our jemalloc package

--- cmake/FindJemalloc.cmake
+++ cmake/FindJemalloc.cmake
@@ -1,40 +1,58 @@
 # - Try to find jemalloc
-# Once done this will define
-#  JEMALLOC_FOUND        - System has jemalloc
-#  JEMALLOC_INCLUDE_DIRS - The jemalloc include directories
-#  JEMALLOC_LIBRARIES    - The libraries needed to use jemalloc
+# Once done this will define a target Jemalloc::jemalloc which will include all
+# required definitions and libraries.
 
-find_package(PkgConfig QUIET)
-pkg_check_modules(PC_JEMALLOC QUIET jemalloc)
+if (NOT FindJemalloc_included)
 
-find_path(JEMALLOC_INCLUDE_DIR
-  NAMES jemalloc/jemalloc.h
-  HINTS ${PC_JEMALLOC_INCLUDE_DIRS}
-)
-find_library(JEMALLOC_LIBRARY
-  NAMES jemalloc
-  HINTS ${PC_JEMALLOC_LIBRARY_DIRS}
-)
+  # First try finding Jemalloc using pkg-config.
+  find_package(PkgConfig QUIET)
+  if (PKG_CONFIG_FOUND)
+    pkg_check_modules(PC_JEMALLOC QUIET IMPORTED_TARGET GLOBAL jemalloc)
 
-if(JEMALLOC_INCLUDE_DIR)
+    if (PC_JEMALLOC_FOUND)
+      # Ensure it created the PkgConfig target
+      if(NOT TARGET PkgConfig::PC_JEMALLOC)
+        message(FATAL_ERROR
+          "Found Jemalloc via pkg-config, but it did not create the PkgConfig::PC_JEMALLOC target.")
+      endif()
+
+      # Make the discovered target available as Jemalloc::jemalloc
+      add_library(Jemalloc::jemalloc ALIAS PkgConfig::PC_JEMALLOC)
+      set(_jemalloc_found TRUE)
+    endif()
+  endif()
+
+  # If that didn't find it, try finding Jemalloc using CMake's config
+  # mode.
+  if(NOT _jemalloc_found)
+    find_package(Jemalloc CONFIG)
+
+    if(Jemalloc_FOUND)
+      if(NOT TARGET Jemalloc::jemalloc)
+        # Ensure this found package created the standard target.
+        message(FATAL_ERROR
+          "Found Jemalloc, but it did not create the Jemalloc::jemalloc target.")
+      endif()
+      set(_jemalloc_found TRUE)
+    endif()
+  endif()
+
+  # Determine the version of the found jemalloc.
+  get_target_property(_jemalloc_include_dirs Jemalloc::jemalloc INTERFACE_INCLUDE_DIRECTORIES)
+  list (GET _jemalloc_include_dirs 0 _jemalloc_include_dir)
   set(_version_regex "^#define[ \t]+JEMALLOC_VERSION[ \t]+\"([^\"]+)\".*")
-  file(STRINGS "${JEMALLOC_INCLUDE_DIR}/jemalloc/jemalloc.h"
+  file(STRINGS "${_jemalloc_include_dir}/jemalloc/jemalloc.h"
     JEMALLOC_VERSION REGEX "${_version_regex}")
   string(REGEX REPLACE "${_version_regex}" "\\1"
     JEMALLOC_VERSION "${JEMALLOC_VERSION}")
   unset(_version_regex)
-endif()
 
-include(FindPackageHandleStandardArgs)
-# handle the QUIETLY and REQUIRED arguments and set JEMALLOC_FOUND to TRUE
-# if all listed variables are TRUE and the requested version matches.
-find_package_handle_standard_args(Jemalloc REQUIRED_VARS
-                                  JEMALLOC_LIBRARY JEMALLOC_INCLUDE_DIR
-                                  VERSION_VAR JEMALLOC_VERSION)
+  # handle the QUIET and REQUIRED arguments, verify version (if
+  # necessary), report found status, and set JEMALLOC_FOUND to TRUE.
+  include(FindPackageHandleStandardArgs)
+  find_package_handle_standard_args(
+    Jemalloc VERSION_VAR JEMALLOC_VERSION
+    REQUIRED_VARS _jemalloc_found)
+  set(FindJemalloc_included TRUE)
 
-if(JEMALLOC_FOUND)
-  set(JEMALLOC_LIBRARIES    ${JEMALLOC_LIBRARY})
-  set(JEMALLOC_INCLUDE_DIRS ${JEMALLOC_INCLUDE_DIR})
-endif()
-
-mark_as_advanced(JEMALLOC_INCLUDE_DIR JEMALLOC_LIBRARY)
+endif (NOT FindJemalloc_included)
