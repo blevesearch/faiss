a0112f985f1dfa2bc965f114d84ecd567abedc58
Rahul Rampure <rahul.rampure@couchbase.com>
2026-06-08 15:11:35 +0530

    Revert "revert2"
    This reverts commit dac87c3022c1b3e7d3b67d29e2be6705c72bf1b6.

--- faiss/gpu/StandardGpuResources.h
+++ faiss/gpu/StandardGpuResources.h
@@ -68,6 +68,11 @@ class StandardGpuResourcesImpl : public GpuResources {
     /// Must be called before any device is initialized.
     void setTempMemorySpace(MemorySpace space);
 
+    /// Set if we should use a dynamic stack that can grow with repeated
+    /// allocations or a single fixed stack. Default is false (single fixed
+    /// stack).
+    void dynamicTempMemory();
+
     /// Set amount of pinned memory to allocate, for async GPU <-> CPU
     /// transfers
     void setPinnedMemory(size_t size);
@@ -198,6 +203,10 @@ class StandardGpuResourcesImpl : public GpuResources {
 
     /// Memory space for temp allocations - pool and overflow (default: Device)
     MemorySpace tempMemorySpace_;
+
+    /// Whether to use a dynamic stack that can grow with repeated allocations
+    /// or a single fixed stack (default: false)
+    bool dynamicTempMemory_;
 };
 
 /// Default implementation of GpuResources that allocates a cuBLAS
@@ -233,6 +242,10 @@ class StandardGpuResources : public GpuResourcesProvider {
     /// Set to MemorySpace::Unified for cudaMallocManaged.
     /// Must be called before any device is initialized.
     void setTempMemorySpace(MemorySpace space);
+
+    /// Set if we should use a dynamic stack that can grow with repeated
+    /// allocations or a single fixed stack
+    void dynamicTempMemory();
 
     /// Set amount of pinned memory to allocate, for async GPU <-> CPU
     /// transfers
