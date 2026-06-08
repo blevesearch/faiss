0175331ef61d9d77dce9c0c00251172975fd6613
Rahul Rampure <rahul.rampure@couchbase.com>
2026-06-09 00:09:13 +0530

    fix 2

--- faiss/gpu/utils/PoolDeviceMemory.cpp
+++ faiss/gpu/utils/PoolDeviceMemory.cpp
@@ -40,7 +40,7 @@ void* PoolDeviceMemory::allocMemory(cudaStream_t stream, size_t size) {
     auto err = cudaMallocFromPoolAsync(&ptr, size, pool_, stream);
 	if (err != cudaSuccess) {
 		cudaGetLastError();
-		FAISS_THROW_IF_NOT_FMT(err == cudaSuccess, "%s", str.c_str());
+		FAISS_THROW_IF_NOT_FMT(err == cudaSuccess, "%s", cudaGetErrorString(err));
 	}
     return ptr;
 }
