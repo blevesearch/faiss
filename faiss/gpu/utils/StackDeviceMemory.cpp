ce5393943eca8f121b1175fb97309e48be4bbf23
Rahul Rampure <rahul.rampure@couchbase.com>
2026-06-08 15:09:50 +0530

    revert

--- faiss/gpu/utils/StackDeviceMemory.cpp
+++ faiss/gpu/utils/StackDeviceMemory.cpp
@@ -82,10 +82,6 @@ size_t StackDeviceMemory::Stack::getSizeAvailable() const {
     return (end_ - head_);
 }
 
-bool StackDeviceMemory::Stack::contains(char* p) const {
-    return p >= start_ && p < end_;
-}
-
 char* StackDeviceMemory::Stack::getAlloc(size_t size, cudaStream_t stream) {
     // The user must check to see that the allocation fit within us
     auto sizeRemaining = getSizeAvailable();
@@ -176,76 +172,12 @@ std::string StackDeviceMemory::Stack::toString() const {
     return s.str();
 }
 
-StackDeviceMemory::DynamicStack::DynamicStack(
-        GpuResources* res,
-        int device,
-        MemorySpace space)
-        : res_(res), device_(device), tempMemorySpace_(space) {}
-
-StackDeviceMemory::DynamicStack::~DynamicStack() {}
-
-size_t StackDeviceMemory::DynamicStack::getSizeAvailable() const {
-    return std::numeric_limits<size_t>::max();
-}
-
-char* StackDeviceMemory::DynamicStack::getAlloc(
-        size_t size,
-        cudaStream_t stream) {
-    void* ptr = nullptr;
-    auto err = cudaMallocAsync(&ptr, size, stream);
-    if (err != cudaSuccess) {
-        cudaGetLastError();
-        FAISS_THROW_IF_NOT_FMT(err == cudaSuccess, "%s", cudaGetErrorString(err));
-    }
-
-    size_t prev = bytesOutstanding_.fetch_add(size, std::memory_order_relaxed);
-    highWaterBytesOutstanding_ =
-            std::max(highWaterBytesOutstanding_, prev + size);
-
-    return static_cast<char*>(ptr);
-}
-
-void StackDeviceMemory::DynamicStack::returnAlloc(
-        char* p,
-        size_t size,
-        cudaStream_t stream) {
-    for (auto& arena : arenas_) {
-        if (arena->contains(p)) {
-            arena->returnAlloc(p, size, stream);
-            return;
-        }
-    }
-
-    FAISS_ASSERT_FMT(false, "unknown dynamic stack allocation %p", p);
-}
-
-std::string StackDeviceMemory::DynamicStack::toString() const {
-    std::stringstream s;
-
-    s << "Dynamic SDM device " << device_ << ": " << arenas_.size()
-      << " arenas\n";
-
-    for (size_t i = 0; i < arenas_.size(); ++i) {
-        s << "Arena " << i << ":\n" << arenas_[i]->toString();
-    }
-
-    return s.str();
-}
-
 StackDeviceMemory::StackDeviceMemory(
         GpuResources* res,
         int device,
         size_t allocPerDevice,
         MemorySpace space)
-        : device_(device),
-          stack_(std::make_unique<Stack>(res, device, allocPerDevice, space)) {}
-
-StackDeviceMemory::StackDeviceMemory(
-        GpuResources* res,
-        int device,
-        MemorySpace space)
-        : device_(device),
-          dynamicStack_(std::make_unique<DynamicStack>(res, device, space)) {}
+        : device_(device), stack_(res, device, allocPerDevice, space) {}
 
 StackDeviceMemory::~StackDeviceMemory() {}
 
@@ -254,19 +186,17 @@ int StackDeviceMemory::getDevice() const {
 }
 
 size_t StackDeviceMemory::getSizeAvailable() const {
-    return dynamicStack_ ? dynamicStack_->getSizeAvailable()
-                         : stack_->getSizeAvailable();
+    return stack_.getSizeAvailable();
 }
 
 std::string StackDeviceMemory::toString() const {
-    return dynamicStack_ ? dynamicStack_->toString() : stack_->toString();
+    return stack_.toString();
 }
 
 void* StackDeviceMemory::allocMemory(cudaStream_t stream, size_t size) {
     // All allocations should have been adjusted to a multiple of 16 bytes
     FAISS_ASSERT(size % 16 == 0);
-    return dynamicStack_ ? dynamicStack_->getAlloc(size, stream)
-                         : stack_->getAlloc(size, stream);
+    return stack_.getAlloc(size, stream);
 }
 
 void StackDeviceMemory::deallocMemory(
@@ -277,8 +207,7 @@ void StackDeviceMemory::deallocMemory(
     FAISS_ASSERT(p);
     FAISS_ASSERT(device == device_);
 
-    dynamicStack_ ? dynamicStack_->returnAlloc((char*)p, size, stream)
-                  : stack_->returnAlloc((char*)p, size, stream);
+    stack_.returnAlloc((char*)p, size, stream);
 }
 
 } // namespace gpu
